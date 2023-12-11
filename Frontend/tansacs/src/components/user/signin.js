02f2781b79ae3c8ffd2e2762f4cfd482aa23745f
Ajith Kumar R <ajithkumardotr@gmail.com>
2023-12-11 22:45:57 +0530

    project fully completed 100 percentage

--- Frontend/tansacs/src/components/user/signin.js
+++ Frontend/tansacs/src/components/user/signin.js
@@ -192,8 +192,8 @@ function SignIn(props) {
                         </Formik>
                     </div>
 
-                    <DownloadButton file_name="Tansacsjobcriteria.pdf" content="TANSACS Recruitment Eligibility Criteria.pdf" />
-                    <DownloadButton file_name="NOC Form.pdf" content="NOC Form.pdf" />
+                    <DownloadButton file_name="TANSACS recruitment.pdf" content="TANSACS Recruitment Eligibility Criteria.pdf" />
+                    <DownloadButton file_name="NOC_Format.pdf" content="NOC Form.pdf" />
                     <p className='text-custom-red font-bold mb-3 font6'>Note: TANSACS Job Posting Details are given in the link above, please read carefully before applying.</p>
                     <p className='font-semibold text-lg font6'>JOB POSTING WILL BE ALLOCATED PURELY BASED ON THE CRITERIA.</p>
 
